4554762b50ccaab36f23361a23f87d1828a8f296
natsuki <synchro0718@icloud.com>
2022-08-30 00:14:21 +0900

    create function of Sending data to google sheet

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,20 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:vantan_connect/view/initial_page.dart';
+import 'package:vantan_connect/api/gsheet.dart';
+import 'package:vantan_connect/view/home/home_page.dart';
 import 'firebase_options.dart';
 import 'const/color_schemes.g.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await gSheetInit();
   runApp(const MyApp());
 }
+
+//TODO formfieldのfocuscolorは変えない。太さを変える
+//TODO 目のアイコンは青にしたい。同時にhideを変更したい
+//TODO 左下を基準にしたい
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -18,8 +24,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      theme: changeColorOnDarkMode(ThemeData(brightness: Brightness.light)),
-      home: const InitialPage(),
+      theme: changeColorOnDarkMode(
+        ThemeData(brightness: Brightness.light),
+      ),
+      home: const HomePage(),
     );
   }
 }
