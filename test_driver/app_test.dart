cc79d6c6755b052f617fd9a886cd3c4b75c7d18c
natsuki <synchro0718@icloud.com>
2022-10-02 14:09:06 +0900

    🗑 delete unused test code

--- test_driver/app_test.dart
+++ test_driver/app_test.dart
@@ -7,13 +7,4 @@ void main() {
   setUpAll(() async {
     driver = await FlutterDriver.connect();
   });
-
-  test('Counter increments smoke test', () async {
-    await driver.tap(find.byValueKey('loginKey'));
-    await driver.enterText('test@test.jp');
-    await driver.tap(find.byValueKey('passwordKey'));
-    await driver.enterText('testtest');
-    await driver.tap(find.text('ログインする'));
-    await driver.waitUntilNoTransientCallbacks();
-  });
 }
