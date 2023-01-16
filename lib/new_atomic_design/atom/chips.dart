646caea6321a68ed28a9bb43e27f91cc27cb637b
natsuki <synchro0718@icloud.com>
2023-01-16 22:42:29 +0900

    add: atom

---
+++ lib/new_atomic_design/atom/chips.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:vantan_connect/view/token/space_box.dart';
+import 'package:vantan_connect/view/token/style_by_platform.dart';
+
+class SelectedChips extends StatelessWidget {
+  const SelectedChips({
+    Key? key,
+    required this.text,
+    required this.textStyle,
+    required this.padding,
+    required this.backgroundColor,
+    required this.borderColor,
+  }) : super(key: key);
+
+  final String text;
+  final TextStyle textStyle;
+  final EdgeInsets padding;
+  final Color backgroundColor;
+  final Color borderColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: padding,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(8.sp),
+      ),
+      child: Text(text, style: textStyle),
+    );
+  }
+}
+
+class UnSelectedChips extends StatelessWidget {
+  const UnSelectedChips({
+    Key? key,
+    required this.text,
+    required this.textColor,
+    required this.padding,
+    required this.backgroundColor,
+  }) : super(key: key);
+
+  final String text;
+  final Color textColor;
+  final EdgeInsets padding;
+  final Color backgroundColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: padding,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(8.sp),
+        color: backgroundColor,
+      ),
+      child: Row(
+        children: [
+          Icon(Icons.add, size: 12.sp, color: textColor),
+          SpaceBox(width: 10.sp),
+          Text(text, style: bodyBold(textColor)),
+        ],
+      ),
+    );
+  }
+}
