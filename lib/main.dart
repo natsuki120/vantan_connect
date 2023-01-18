c12195ecb997e66f5200245b1605e6d8443c492c
natsuki <synchro0718@icloud.com>
2023-01-18 09:44:30 +0900

    update: make difference output screens, macOS or not why: in trouble if same screen outputs macOS or not

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:vantan_connect/new_atomic_design/organism/sidebar.dart';
 import 'package:vantan_connect/vantan_life.dart';
-import 'package:vantan_connect/view/template/management_screen_template.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
           debugShowCheckedModeBanner: false,
           title: 'Flutter Demo',
           theme: ThemeData(brightness: Brightness.light),
-          home: Platform.isMacOS ? ManagementScreenTemplate() : VantanLife(),
+          home: Platform.isMacOS ? SideBar() : VantanLife(),
         );
       },
     );
