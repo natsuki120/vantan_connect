4bd3f68c397d2edc463febd5e40508d9f743bb68
natsuki <synchro0718@icloud.com>
2023-01-19 16:44:50 +0900

    update: add calender modal in select dates

--- lib/new_atomic_design/molecule/select_date.dart
+++ lib/new_atomic_design/molecule/select_date.dart
@@ -14,69 +14,67 @@ class SelectDate extends HookWidget {
   @override
   Widget build(BuildContext context) {
     final hover = useState(false);
-    return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          SizedBox(
-            width: 328.sp,
-            child: TextFormField(
-              decoration: InputDecoration(
-                enabledBorder: OutlineInputBorder(),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(width: 2.0.sp, color: primary),
-                ),
-                labelText: 'Date',
-                suffixIcon: MouseRegion(
-                  onHover: (event) => hover.value = true,
-                  child: IconButton(
-                    onPressed: () {},
-                    icon: OriginalIcon(
-                      iconData: Icons.today,
-                      iconSize: 24.sp,
-                      iconColor: midEmphasis,
-                    ),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SizedBox(
+          width: 328.sp,
+          child: TextFormField(
+            decoration: InputDecoration(
+              enabledBorder: OutlineInputBorder(),
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(width: 2.0.sp, color: primary),
+              ),
+              labelText: 'Date',
+              suffixIcon: MouseRegion(
+                onHover: (event) => hover.value = true,
+                child: IconButton(
+                  onPressed: () {},
+                  icon: OriginalIcon(
+                    iconData: Icons.today,
+                    iconSize: 24.sp,
+                    iconColor: midEmphasis,
                   ),
                 ),
               ),
             ),
           ),
-          SpaceBox(height: 7.sp),
-          Padding(
-            padding: EdgeInsets.only(left: 8.0.sp),
-            child: Text(
-              'MM/DD/YYYY',
-              style: bodySmall(FontWeight.w400, highEmphasis),
-            ),
+        ),
+        SpaceBox(height: 7.sp),
+        Padding(
+          padding: EdgeInsets.only(left: 8.0.sp),
+          child: Text(
+            'MM/DD/YYYY',
+            style: bodySmall(FontWeight.w400, highEmphasis),
           ),
-          SpaceBox(height: 7.sp),
-          hover.value
-              ? Container(
-                  width: 400.sp,
-                  height: 523.sp,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(16.sp),
-                    color: Color(0xffE2F6FC),
-                  ),
-                  child: CalendarDatePicker2WithActionButtons(
-                    initialValue: [],
-                    config: CalendarDatePicker2WithActionButtonsConfig(
-                      selectedDayHighlightColor: primary,
-                      okButton: Text(
-                        'OK',
-                        style: labelLarge(FontWeight.w500, primary),
-                      ),
-                      cancelButton: OriginalToggleIconText(
-                        text: 'Cancel',
-                        textStyle: labelLarge(FontWeight.w500, primary),
-                        callback: () => hover.value = false,
-                      ),
+        ),
+        SpaceBox(height: 7.sp),
+        hover.value
+            ? Container(
+                width: 400.sp,
+                height: 523.sp,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(16.sp),
+                  color: Color(0xffE2F6FC),
+                ),
+                child: CalendarDatePicker2WithActionButtons(
+                  initialValue: [],
+                  config: CalendarDatePicker2WithActionButtonsConfig(
+                    selectedDayHighlightColor: primary,
+                    okButton: Text(
+                      'OK',
+                      style: labelLarge(FontWeight.w500, primary),
+                    ),
+                    cancelButton: OriginalToggleIconText(
+                      text: 'Cancel',
+                      textStyle: labelLarge(FontWeight.w500, primary),
+                      callback: () => hover.value = false,
                     ),
                   ),
-                )
-              : Container(),
-        ],
-      ),
+                ),
+              )
+            : Container(),
+      ],
     );
   }
 }
