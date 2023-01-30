b01c51cf19aec9778610128b1b624e1d41f1f374
issei <issei.hiramatsu@gmail.com>
2023-01-30 11:11:10 +0900

    update: add Hooks in radio_button

---
+++ lib/component/local/attendance_modal_with_reason/elements/radio_button_with_attendance_status/hooks/use.dart
@@ -0,0 +1,44 @@
+enum AttendanceState {
+  tardy('遅刻'),
+  absence('欠席'),
+  other('その他(公欠を除く');
+
+  final String displayState;
+  const AttendanceState(this.displayState);
+}
+
+
+// AttendanceState? useAttendanceState(BuildContext context) {
+//   return use(_AttendanceState());
+// }
+
+// class _AttendanceState extends Hook<AttendanceState?> {
+//   @override
+//   __AttendanceStateState createState() => __AttendanceStateState();
+// }
+
+// class __AttendanceStateState
+//     extends HookState<AttendanceState?, _AttendanceState> {
+//   //int _number = 0;
+//   AttendanceState? _attendanceState = AttendanceState.absence;
+
+//   // @override
+//   // void initHook() {
+//   //   super.initHook();
+//   //   (_value) {
+//   //     setState(() {
+//   //       _attendanceState = _value;
+//   //     });
+//   //   };
+//   // }
+
+//   @override
+//   AttendanceState? build(BuildContext context) {
+//     return _attendanceState;
+//   }
+
+//   @override
+//   void dispose() {
+//     super.dispose();
+//   }
+// }
