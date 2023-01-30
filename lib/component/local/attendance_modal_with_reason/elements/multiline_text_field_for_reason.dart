f7f35cb7328e1dc13d8150be29165a0d4a866b56
issei <issei.hiramatsu@gmail.com>
2023-01-30 14:24:58 +0900

    Fix: TextEditingControlelr change to useTextEditingController

--- lib/component/local/attendance_modal_with_reason/elements/multiline_text_field_for_reason.dart
+++ lib/component/local/attendance_modal_with_reason/elements/multiline_text_field_for_reason.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:vantan_connect/component/shered/single/color.dart';
 
-class MultilineTextFieldForReason extends StatelessWidget {
-  const MultilineTextFieldForReason({super.key});
+class MultilineTextFieldForReason extends HookWidget {
+  MultilineTextFieldForReason({super.key});
 
+  final myController = useTextEditingController();
   @override
   Widget build(BuildContext context) {
     return TextField(
       keyboardType: TextInputType.multiline,
+      controller: myController,
       maxLines: 4,
       decoration: InputDecoration(
           border: OutlineInputBorder(
