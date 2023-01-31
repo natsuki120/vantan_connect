3b5f92900b168d94d819950f0390e04990d65f64
natsuki <synchro0718@icloud.com>
2023-01-31 21:25:49 +0900

    update: lesson_detail_header.dart,timetable_content.dart why: fix design

--- lib/component/local/timetable_content/timetable_content.dart
+++ lib/component/local/timetable_content/timetable_content.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:vantan_connect/component/local/timetable_content/element/timetable_content_case_of_applied_class.dart';
-import 'package:vantan_connect/domain/class_time.dart';
 import '../../shered/single/space_box.dart';
 import 'element/element/timetable_header_column.dart';
 import 'hook.dart';
@@ -17,16 +16,27 @@ class TimeTableContent extends StatelessWidget {
         child: Row(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Column(
-              children: [
-                SpaceBox(height: 45.sp),
-                for (ClassTime classStartTime in classStartTimeList)
-                  UnSelectedTimeTableHeaderColumn(
-                    start: classStartTime.start,
-                    timeTable: 1,
-                    end: classStartTime.end,
-                  ),
-              ],
+            Expanded(
+              child: Column(
+                children: [
+                  ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: classStartTimeList.length,
+                      itemBuilder: (context, index) {
+                        final classStartTime = classStartTimeList[index];
+                        return Column(
+                          children: [
+                            UnSelectedTimeTableHeaderColumn(
+                              start: classStartTime.start,
+                              timeTable: index + 1,
+                              end: classStartTime.end,
+                            ),
+                            SpaceBox(height: 4.sp),
+                          ],
+                        );
+                      }),
+                ],
+              ),
             ),
             SpaceBox(width: 9.sp),
             // 月〜金まで日程の表示を繰り返す
