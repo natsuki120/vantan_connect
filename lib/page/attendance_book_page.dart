925e668a9f8203fbcedafc921638d9e2d1507022
natsuki <synchro0718@icloud.com>
2023-02-03 14:02:09 +0900

    update: attendance_book_page.dart why: provider name is same with provider which fetch student attendance provider

--- lib/page/attendance_book_page.dart
+++ lib/page/attendance_book_page.dart
@@ -14,7 +14,7 @@ class AttendanceBookPage extends ConsumerWidget {
     return Scaffold(
       body: Container(
         alignment: Alignment.center,
-        child: ref.watch(queryService(classDocument)).when(
+        child: ref.watch(testProvider(classDocument)).when(
               data: (data) => Text(data.toString()),
               error: (error, _) => Text(error.toString()),
               loading: () => const CircularProgressIndicator(),
