cbfb3143f4393e86e2f2a9daa705bd3013c9a2d2
issei <issei.hiramatsu@gmail.com>
2023-02-05 15:55:51 +0900

    update: add Hero Widget

--- lib/component/local/grades_table/grades_table_body/elements/todays_lesson/todays_lesson_body.dart
+++ lib/component/local/grades_table/grades_table_body/elements/todays_lesson/todays_lesson_body.dart
@@ -1,49 +1,29 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-import '../../../../../shared/single/color.dart';
-import 'elements/todays_lesson_list.dart';
+import 'elements/todays_lesson_tab/todays_lesson_tab.dart';
+import 'elements/todays_lesson_tab_bar_view.dart';
 
-class TodaysLessonBody extends HookWidget {
+class TodaysLessonBody extends StatelessWidget {
   TodaysLessonBody({super.key}) : super();
 
   @override
   Widget build(BuildContext context) {
-    final _todaysLessonTabController = useTabController(initialLength: 3);
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Row(
           children: [
             SizedBox(width: 20),
-            TabBar(
-              controller: _todaysLessonTabController,
-              indicator: BoxDecoration(
-                borderRadius: BorderRadius.circular(8), // Creates border
-                color: primary,
-              ),
-              isScrollable: true,
-              labelColor: white,
-              unselectedLabelColor: midEmphasis,
-              tabs: [
-                Tab(text: 'aクラス', height: 28.sp),
-                Tab(text: 'bクラス', height: 28.sp),
-                Tab(text: 'cクラス', height: 28.sp),
-              ],
-            ),
+            TodaysLessonTab(),
           ],
         ),
         SizedBox(height: 16.sp),
         Container(
           height: 283.sp,
-          child: TabBarView(
-            controller: _todaysLessonTabController,
-            children: [
-              TodaysLessonList(),
-              TodaysLessonList(),
-              TodaysLessonList(),
-            ],
+          child: Hero(
+            tag: 'grades_table_modal_show_todays_lessons',
+            child: TodaysLessonTabBarView(),
           ),
         ),
       ],
