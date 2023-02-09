2fe1008fc3a460e1c42705ae9eece9c289166ac2
natsuki <synchro0718@icloud.com>
2023-02-10 00:41:28 +0900

    update: student_list_lesson_detail_page.dart how: can scroll

--- lib/page/student_list_lesson_detail_page.dart
+++ lib/page/student_list_lesson_detail_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:vantan_connect/component/local/attendance_record/attendance_record.dart';
 import 'package:vantan_connect/domain/student/student.dart';
 import '../component/shared/single/color.dart';
@@ -13,37 +14,70 @@ class StudentListInLessonDetailPage extends ConsumerWidget {
     return DefaultTabController(
       length: 2,
       child: Scaffold(
-        appBar: AppBar(
-          bottom: TabBar(
-            unselectedLabelStyle: bodyRegular(midEmphasis),
-            labelStyle: bodyBold(primary),
-            labelColor: primary,
-            indicatorColor: primary,
-            indicatorSize: TabBarIndicatorSize.tab,
-            tabs: [
-              Tab(text: '授業詳細'),
-              Tab(text: '出席記録'),
+        appBar: AppBar(),
+        body: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                height: 114,
+                padding: EdgeInsets.symmetric(horizontal: 16.sp),
+                alignment: Alignment.centerLeft,
+                child: Text('UI/UXとは', style: header(highEmphasis)),
+              ),
+              SizedBox(
+                height: 100,
+                child: AppBar(
+                  bottom: TabBar(
+                    unselectedLabelStyle: bodyRegular(midEmphasis),
+                    labelStyle: bodyBold(primary),
+                    labelColor: primary,
+                    indicatorColor: primary,
+                    indicatorSize: TabBarIndicatorSize.tab,
+                    tabs: [
+                      Tab(text: '授業詳細'),
+                      Tab(text: '出席記録'),
+                    ],
+                  ),
+                ),
+              ),
+              SingleChildScrollView(
+                child: SizedBox(
+                  height: 850,
+                  child: TabBarView(
+                    children: [
+                      Container(),
+                      AttendanceRecord(
+                        studentList: [
+                          Student(name: '高橋夏輝'),
+                          Student(name: '高橋夏輝'),
+                          Student(name: '高橋夏輝'),
+                          Student(name: '高橋夏輝'),
+                          Student(name: '高橋夏輝'),
+                          Student(name: '高橋夏輝'),
+                          Student(name: '高橋夏輝'),
+                          Student(name: '高橋夏輝'),
+                          Student(name: '高橋夏輝'),
+                          Student(name: '高橋夏輝'),
+                          Student(name: '高橋夏輝'),
+                          Student(name: '高橋夏輝'),
+                          Student(name: '高橋夏輝'),
+                          Student(name: '高橋夏輝'),
+                          Student(name: '高橋夏輝'),
+                          Student(name: '高橋夏輝'),
+                          Student(name: '高橋夏輝'),
+                          Student(name: '高橋夏輝'),
+                          Student(name: '高橋夏輝'),
+                          Student(name: '高橋夏輝'),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             ],
           ),
-        ),
-        body: TabBarView(
-          children: [
-            Container(),
-            AttendanceRecord(
-              studentList: [
-                Student(name: '高橋夏輝'),
-                Student(name: '高橋夏輝'),
-                Student(name: '高橋夏輝'),
-                Student(name: '高橋夏輝'),
-                Student(name: '高橋夏輝'),
-                Student(name: '高橋夏輝'),
-                Student(name: '高橋夏輝'),
-                Student(name: '高橋夏輝'),
-                Student(name: '高橋夏輝'),
-                Student(name: '高橋夏輝'),
-              ],
-            ),
-          ],
         ),
       ),
     );
