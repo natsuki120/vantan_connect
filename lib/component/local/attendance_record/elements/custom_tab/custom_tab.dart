096b45998b290076668e7388e38304ecc5d7d6fa
natsuki <synchro0718@icloud.com>
2023-02-10 00:41:47 +0900

    add: custom_tab.dart

---
+++ lib/component/local/attendance_record/elements/custom_tab/custom_tab.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import '../../../../shared/single/color.dart';
+import '../../../../shared/single/text_style.dart';
+
+class CustomTab extends StatelessWidget implements PreferredSizeWidget {
+  const CustomTab({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      bottom: TabBar(
+        isScrollable: true,
+        indicator: BoxDecoration(
+          borderRadius: BorderRadius.circular(8.sp),
+          color: primary,
+        ),
+        labelColor: white,
+        unselectedLabelColor: midEmphasis,
+        labelStyle: bodyBold(white),
+        unselectedLabelStyle: bodyRegular(midEmphasis.withOpacity(0.6)),
+        tabs: [
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 15.sp),
+            child: Tab(text: 'すべて', height: 32.sp),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 15.sp),
+            child: Tab(text: '出席', height: 32.sp),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 15.sp),
+            child: Tab(text: '欠席', height: 32.sp),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 15.sp),
+            child: Tab(text: 'その他', height: 32.sp),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => new Size.fromHeight(kToolbarHeight);
+}
