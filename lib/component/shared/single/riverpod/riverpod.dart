7a364f8e22e5fb61c82e0387f47f1b98486de9f3
natsuki <synchro0718@icloud.com>
2023-02-11 23:12:21 +0900

    delete: 生徒の出席状況を取得する関数の引数にClassDocument型を追加
    単一授業の詳細を取得するように関数にClassDocument型を追加した。

--- lib/component/shared/single/riverpod/riverpod.dart
+++ lib/component/shared/single/riverpod/riverpod.dart
@@ -1,24 +1,13 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:vantan_connect/domain/class/class.dart';
-
+import 'package:vantan_connect/domain/riverpod_argument/class_and_document/class_and_document.dart';
 import 'package:vantan_connect/domain/student_attendance/student_attendance.dart';
-import '/domain/class_document/class_document.dart';
-import '/domain/student/student.dart';
 import '/infrastructure/student_repository.dart';
 import '/infrastructure/query_service_repository.dart';
 import '/use_case/actor/student.dart';
 import '../../../../use_case/query_service/query_service.dart';
 
 final studentUseCase = Provider((ref) => StudentUseCase(StudentRepository()));
-
-final classInfoProvider =
-    FutureProvider.family.autoDispose<List<Student>, ClassDocument>(
-  (ref, ClassDocument classDocument) async {
-    return await QueryServiceUseCase(
-      queryService: QueryServiceRepositoryWhichUseFirebase(),
-    ).fetchClassInfo(classDocument);
-  },
-);
 
 final queryServiceProvider = StreamProvider.family<dynamic, StudentAttendance>(
   (ref, studentAttendance) => QueryServiceUseCase(
@@ -29,28 +18,44 @@ final queryServiceProvider = StreamProvider.family<dynamic, StudentAttendance>(
   ),
 );
 
+final fetchAllClassDocumentByClass = FutureProvider.family(
+  (ref, Class classInfo) => QueryServiceUseCase(
+          queryService: QueryServiceRepositoryWhichUseFirebase())
+      .fetchAllClassDocumentByClass(classInfo: classInfo),
+);
+
 final fetchStudentAttendanceByClass =
-    StreamProvider.family((ref, Class classInfo) {
+    StreamProvider.family((ref, ClassAndDocument classAndDocument) {
   return QueryServiceUseCase(
     queryService: QueryServiceRepositoryWhichUseFirebase(),
-  ).fetchStudentAttendanceByClass(classInfo: classInfo);
+  ).fetchStudentAttendanceByClass(
+      classInfo: classAndDocument.classInfo,
+      classDocument: classAndDocument.classDocument);
 });
 
 final fetchStudentNotAttendanceByClass =
-    StreamProvider.family((ref, Class classInfo) {
+    StreamProvider.family((ref, ClassAndDocument classAndDocument) {
   return QueryServiceUseCase(
     queryService: QueryServiceRepositoryWhichUseFirebase(),
-  ).fetchStudentNotAttendByClass(classInfo: classInfo);
+  ).fetchStudentNotAttendByClass(
+      classInfo: classAndDocument.classInfo,
+      classDocument: classAndDocument.classDocument);
 });
 
-final fetchStudentLateByClass = StreamProvider.family((ref, Class classInfo) {
+final fetchStudentLateByClass =
+    StreamProvider.family((ref, ClassAndDocument classAndDocument) {
   return QueryServiceUseCase(
     queryService: QueryServiceRepositoryWhichUseFirebase(),
-  ).fetchStudentLateByClass(classInfo: classInfo);
+  ).fetchStudentLateByClass(
+      classInfo: classAndDocument.classInfo,
+      classDocument: classAndDocument.classDocument);
 });
 
-final fetchStudentOtherByClass = StreamProvider.family((ref, Class classInfo) {
+final fetchStudentOtherByClass =
+    StreamProvider.family((ref, ClassAndDocument classAndDocument) {
   return QueryServiceUseCase(
     queryService: QueryServiceRepositoryWhichUseFirebase(),
-  ).fetchStudentOtherByClass(classInfo: classInfo);
+  ).fetchStudentOtherByClass(
+      classInfo: classAndDocument.classInfo,
+      classDocument: classAndDocument.classDocument);
 });
