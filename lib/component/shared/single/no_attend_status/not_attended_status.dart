261c1b07de083d1fed46a7b6dcc7b128b702e462
natsuki <synchro0718@icloud.com>
2023-02-11 14:59:33 +0900

    add: attendance student components

---
+++ lib/component/shared/single/no_attend_status/not_attended_status.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import '../color/color.dart';
+import '../text_style/text_style.dart';
+
+class NotAttendedStatus extends StatelessWidget {
+  const NotAttendedStatus({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 46.sp,
+      height: 28.sp,
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+        color: tertiaryPale,
+        borderRadius: BorderRadius.circular(4.sp),
+      ),
+      child: Text('欠席', style: bodyRegular(midEmphasis)),
+    );
+  }
+}
