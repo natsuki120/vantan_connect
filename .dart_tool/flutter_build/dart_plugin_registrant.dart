ba50f499e11a033c59bdabb6f00fceeadc46830d
natsuki <synchro0718@icloud.com>
2023-02-11 23:15:07 +0900

    update: 依存関係更新

--- .dart_tool/flutter_build/dart_plugin_registrant.dart
+++ .dart_tool/flutter_build/dart_plugin_registrant.dart
@@ -11,9 +11,10 @@ import 'package:image_picker_android/image_picker_android.dart';
 import 'package:path_provider_android/path_provider_android.dart';
 import 'package:geolocator_apple/geolocator_apple.dart';
 import 'package:image_picker_ios/image_picker_ios.dart';
-import 'package:path_provider_ios/path_provider_ios.dart';
+import 'package:path_provider_foundation/path_provider_foundation.dart';
 import 'package:path_provider_linux/path_provider_linux.dart';
-import 'package:path_provider_macos/path_provider_macos.dart';
+import 'package:geolocator_apple/geolocator_apple.dart';
+import 'package:path_provider_foundation/path_provider_foundation.dart';
 import 'package:path_provider_windows/path_provider_windows.dart';
 
 @pragma('vm:entry-point')
@@ -74,10 +75,10 @@ class _PluginRegistrant {
       }
 
       try {
-        PathProviderIOS.registerWith();
+        PathProviderFoundation.registerWith();
       } catch (err) {
         print(
-          '`path_provider_ios` threw an error: $err. '
+          '`path_provider_foundation` threw an error: $err. '
           'The app may not function as expected until you remove this plugin from pubspec.yaml'
         );
         rethrow;
@@ -96,10 +97,20 @@ class _PluginRegistrant {
 
     } else if (Platform.isMacOS) {
       try {
-        PathProviderMacOS.registerWith();
+        GeolocatorApple.registerWith();
       } catch (err) {
         print(
-          '`path_provider_macos` threw an error: $err. '
+          '`geolocator_apple` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+        rethrow;
+      }
+
+      try {
+        PathProviderFoundation.registerWith();
+      } catch (err) {
+        print(
+          '`path_provider_foundation` threw an error: $err. '
           'The app may not function as expected until you remove this plugin from pubspec.yaml'
         );
         rethrow;
