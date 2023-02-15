40e31bb2a0e1bed3098e8d25d27374f3aaa346bc
natsuki <synchro0718@icloud.com>
2023-02-16 02:53:46 +0900

    add: 全ての生徒を取得する画面を作成
    出席、欠席問わず全履修生徒を表示する画面を作成した。

--- lib/component/local/attendance_record/attendance_record.dart
+++ lib/component/local/attendance_record/attendance_record.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:vantan_connect/component/local/attendance_record/elements/all_student_status_list/all_student_status_list.dart';
 import 'package:vantan_connect/domain/class_document/class_document.dart';
 import '../../../domain/class/class.dart';
 import 'elements/attended_student_list/attended_student_list.dart';
@@ -26,7 +27,7 @@ class AttendanceRecord extends HookConsumerWidget {
           padding: EdgeInsets.symmetric(horizontal: 16.sp, vertical: 28.sp),
           child: TabBarView(
             children: [
-              AttendedStudentList(
+              AllStudentList(
                 classInfo: classInfo,
                 classDocument: classDocument,
               ),
---
+++ lib/component/local/attendance_record/elements/all_student_status_list/all_student_status_list.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:vantan_connect/component/shared/combined/attended_student/attended_student.dart';
+import 'package:vantan_connect/component/shared/combined/late_attended_student/late_attended_student.dart';
+import 'package:vantan_connect/component/shared/combined/other_student/other_student.dart';
+import 'package:vantan_connect/component/shared/single/riverpod/riverpod.dart';
+import 'package:vantan_connect/domain/class_document/class_document.dart';
+import '../../../../../domain/class/class.dart';
+import '../../../../../domain/student/student.dart';
+import '../../../../shared/combined/not_attended_student/not_attended_student.dart';
+import '../../../../shared/single/student_list/student_list.dart';
+
+class AllStudentList extends ConsumerWidget {
+  const AllStudentList(
+      {Key? key, required this.classInfo, required this.classDocument})
+      : super(key: key);
+
+  final ClassDocument classDocument;
+  final Class classInfo;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return ref.watch(fetchAllStudentAttendanceStatusByClass(classInfo)).when(
+          data: (dataList) => ListView.builder(
+              shrinkWrap: true,
+              itemCount: studentList.length,
+              itemBuilder: (context, index) {
+                final student = studentList[index];
+                for (Student data in dataList)
+                  if (data.name == student.name) {
+                    if (data.attendanceState == '出席' ||
+                        data.attendanceState == '遅刻') {
+                      if (data.attendanceState == '出席')
+                        return AttendedStudent(student: student);
+                      else
+                        return LateAttendedStudent(student: student);
+                    } else if (data.attendanceState == '欠席') {
+                      return NotAttendedStudent(student: student);
+                    } else {
+                      return OtherStudent(student: student);
+                    }
+                  }
+              }),
+          error: (error, _) => Icon(Icons.error),
+          loading: () => CircularProgressIndicator(),
+        );
+  }
+}
