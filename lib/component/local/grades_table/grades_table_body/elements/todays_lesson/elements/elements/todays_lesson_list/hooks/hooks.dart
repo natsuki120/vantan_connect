810cbdbd06955c01ff6d2175dc4b7ebd811a560c
natsuki <synchro0718@icloud.com>
2023-02-16 01:05:02 +0900

    fix: Cクラスの授業を取得できるように修正

--- lib/component/local/grades_table/grades_table_body/elements/todays_lesson/elements/elements/todays_lesson_list/hooks/hooks.dart
+++ lib/component/local/grades_table/grades_table_body/elements/todays_lesson/elements/elements/todays_lesson_list/hooks/hooks.dart
@@ -3,12 +3,12 @@ import 'package:vantan_connect/domain/class_document/class_document.dart';
 import 'package:vantan_connect/infrastructure/query_service_repository.dart';
 import 'package:vantan_connect/use_case/query_service/query_service.dart';
 
-final fetchClassListInAPClass = StreamProvider(
+final fetchClassListInCClass = StreamProvider(
   (ref) {
     return QueryServiceUseCase(
       queryService: QueryServiceRepositoryWhichUseFirebase(),
-    ).fetchClassListInAPClass();
+    ).fetchClassListInCClass();
   },
 );
 
-final today = FutureProvider((ref) => ClassDocument(day: '2.3'));
+final today = FutureProvider((ref) => ClassDocument(day: '2月16日'));
