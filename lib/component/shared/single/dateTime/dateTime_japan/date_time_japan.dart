faeb70d43ed62130e8b47e36ccb19942f6edf47f
issei <issei.hiramatsu@gmail.com>
2023-02-20 17:55:46 +0900

    update: datetime初期値を設定

--- lib/component/shared/single/dateTime/dateTime_japan/date_time_japan.dart
+++ lib/component/shared/single/dateTime/dateTime_japan/date_time_japan.dart
@@ -5,49 +5,49 @@ import 'package:vantan_connect/component/shared/single/dateTime/check_leap_year.
 
 //とりあえず代用的なものだけを追加しました。必要に応じて足してください
 
-final yearProvider = StreamProvider<String>((ref) => randomValueStream());
-
-Stream<String> randomValueStream() async* {
+final yearProvider = StreamProvider<String>((ref) async* {
   initializeDateFormatting('ja');
+  yield DateFormat.y('ja').format(DateTime.now());
   if (checkLeapYear(DateTime.now()) == true) {
-    Stream.periodic(
+    yield* Stream.periodic(
       Duration(days: 366),
       (_) => DateFormat.y('ja').format(DateTime.now()),
       //表示例　 2000年 ※うるう年
     );
   } else {
-    Stream.periodic(
+    yield* Stream.periodic(
       Duration(days: 365),
       (_) => DateFormat.y('ja').format(DateTime.now()),
       //表示例　 2023年　※うるう年ではない場合
     );
   }
-}
+});
 
 final monthProvider = StreamProvider<String>((ref) async* {
   initializeDateFormatting('ja');
+  yield DateFormat.M('ja').format(DateTime.now());
   if ((checkLeapYear(DateTime.now()) == true) && (DateTime.now().month == 2)) {
-    Stream.periodic(
+    yield* Stream.periodic(
       Duration(days: 29),
       (_) => DateFormat.M('ja').format(DateTime.now()),
       //表示例　 29日 ※うるう年かつ2月
     );
   } else if ((checkLeapYear(DateTime.now()) == false) &&
       (DateTime.now().month == 2)) {
-    Stream.periodic(
+    yield* Stream.periodic(
       Duration(days: 28),
       (_) => DateFormat.M('ja').format(DateTime.now()),
       //表示例　 28日 ※通常2月
     );
   } else if ((checkLeapYear(DateTime.now()) == false) &&
       (DateTime.now().month == 4 | 6 | 9 | 11)) {
-    Stream.periodic(
+    yield* Stream.periodic(
       Duration(days: 30),
       (_) => DateFormat.M('ja').format(DateTime.now()),
       //表示例　 30日
     );
   } else {
-    Stream.periodic(
+    yield* Stream.periodic(
       Duration(days: 31),
       (_) => DateFormat.M('ja').format(DateTime.now()),
       //表示例　31日
@@ -55,35 +55,40 @@ final monthProvider = StreamProvider<String>((ref) async* {
   }
 });
 
-final dateProvider = StreamProvider<String>((ref) {
+final dateProvider = StreamProvider<String>((ref) async* {
   initializeDateFormatting('ja');
-  return Stream.periodic(
+  yield DateFormat.d('ja').format(DateTime.now());
+  yield* Stream.periodic(
     Duration(days: 1),
     (_) => DateFormat.d('ja').format(DateTime.now()),
     //表示例 15日
   );
 });
 
-final dayProvider = StreamProvider<String>((ref) {
+final dayProvider = StreamProvider<String>((ref) async* {
   initializeDateFormatting('ja');
-  return Stream.periodic(
+  yield DateFormat.EEEE('ja').format(DateTime.now());
+  yield* Stream.periodic(
     Duration(days: 7),
     (_) => DateFormat.EEEE('ja').format(DateTime.now()),
     //表示例　水曜日
   );
 });
 
-final minuteProvider = StreamProvider<String>((ref) {
+final minuteProvider = StreamProvider<String>((ref) async* {
   initializeDateFormatting('ja');
-  return Stream.periodic(
-    Duration(minutes: 45),
+  yield DateFormat.m('ja').format(DateTime.now());
+  yield* Stream.periodic(
+    Duration(minutes: 1),
     (_) => DateFormat.m('ja').format(DateTime.now()),
     //表示例 45 ※ 分は含まれない
   );
 });
-final secondProvider = StreamProvider<String>((ref) {
+
+final secondProvider = StreamProvider<String>((ref) async* {
   initializeDateFormatting('ja');
-  return Stream.periodic(
+  yield DateFormat.s('ja').format(DateTime.now());
+  yield* Stream.periodic(
     Duration(seconds: 1),
     (_) => DateFormat.s('ja').format(DateTime.now()),
     //表示例 15   ※秒は含まれない
