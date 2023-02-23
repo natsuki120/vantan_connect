a23e0207c7570ef1c540b16c56d44e085e56160d
natsuki <synchro0718@icloud.com>
2023-02-23 14:40:35 +0900

    fix: 名前が取得できない不具合を修正

--- lib/component/local/login/hooks/hooks.dart
+++ lib/component/local/login/hooks/hooks.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import '../../../../domain/student/student.dart';
 import '../../../../infrastructure/query_service/query_service_repository.dart';
 import '../../../../page/grades_table_page.dart';
 import '../../../../page/test_app.dart';
@@ -13,22 +12,26 @@ Future<void> loginProcess(
     required String name,
     required BuildContext context,
     required int currentSection}) async {
-  EasyLoading.show(status: '読み込み中');
-  final prefs = await SharedPreferences.getInstance();
-  if (ref.watch(myAccountProvider.notifier).state.runtimeType == Student) {
-    ref.watch(myAccountProvider.notifier).state =
-        await QueryServiceRepositoryWhichUseFirebase()
-            .identifyStudent(name: name);
-  } else {
-    await QueryServiceRepositoryWhichUseFirebase().identifyStuff(name: name);
+  try {
+    EasyLoading.show(status: '読み込み中');
+    final prefs = await SharedPreferences.getInstance();
+    try {
+      ref.watch(myAccountProvider.notifier).state =
+          await QueryServiceRepositoryWhichUseFirebase()
+              .identifyStudent(name: name);
+    } catch (e) {
+      await QueryServiceRepositoryWhichUseFirebase().identifyStuff(name: name);
+    }
+    prefs.setString('name', ref.watch(myAccountProvider.notifier).state.name);
+    EasyLoading.showSuccess('識別しました');
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(
+        builder: (BuildContext context) =>
+            currentSection == 0 ? TestApp() : GradesTablePage(),
+      ),
+    );
+  } catch (e) {
+    EasyLoading.showError('存在しないユーザーです');
   }
-  prefs.setString('name', ref.watch(myAccountProvider.notifier).state.name);
-  EasyLoading.showSuccess('識別しました');
-  Navigator.pushReplacement(
-    context,
-    MaterialPageRoute(
-      builder: (BuildContext context) =>
-          currentSection == 0 ? TestApp() : GradesTablePage(),
-    ),
-  );
 }
