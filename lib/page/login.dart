b24fa8555034eeab9c938331de566e7105c1348d
natsuki <synchro0718@icloud.com>
2023-02-23 10:58:24 +0900

    fix: 説明変数を削除
    説明変数に値を代入してもグローバルに定義した方には反映しないため

--- lib/page/login.dart
+++ lib/page/login.dart
@@ -1,5 +1,6 @@
 import 'package:dropdown_textfield/dropdown_textfield.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -10,6 +11,7 @@ import 'package:vantan_connect/component/shared/single/space_box/space_box.dart'
 import 'package:vantan_connect/component/shared/single/student_list/student_list.dart';
 import 'package:vantan_connect/domain/student/student.dart';
 import 'package:vantan_connect/infrastructure/query_service/query_service_repository.dart';
+import 'package:vantan_connect/page/grades_table_page.dart';
 import 'package:vantan_connect/page/test_app.dart';
 import '../component/shared/single/color/color.dart';
 import '../component/shared/single/text_style/text_style.dart';
@@ -19,7 +21,8 @@ class Login extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    Student myAccount = ref.watch(myAccountProvider.notifier).state;
+    String name = '';
+
     final _currentSelection = useState(0);
     Map<int, Widget> _children = {
       0: Text('生徒'),
@@ -59,10 +62,7 @@ class Login extends HookConsumerWidget {
                   border: OutlineInputBorder(),
                   labelText: 'ユーザーを選択',
                 ),
-                // initialValue: "name4",
                 clearOption: true,
-                // enableSearch: true,
-                // dropdownColor: Colors.green,
                 searchDecoration: const InputDecoration(
                     hintText: "enter your custom hint text here"),
                 validator: (value) {
@@ -73,13 +73,18 @@ class Login extends HookConsumerWidget {
                   }
                 },
                 dropDownItemCount: 6,
-                dropDownList: studentList
-                    .map((student) => DropDownValueModel(
-                        name: student.name, value: student.name))
-                    .toList(),
-                onChanged: (val) async {
-                  myAccount = await QueryServiceRepositoryWhichUseFirebase()
-                      .identifyStudent(name: val.name);
+                dropDownList: _currentSelection.value == 0
+                    ? studentList
+                        .map((student) => DropDownValueModel(
+                            name: student.name, value: student.name))
+                        .toList()
+                    : [DropDownValueModel(name: '上原さん', value: '上原さん')],
+                onChanged: (val) {
+                  if (name == '') {
+                    name = '';
+                  } else {
+                    name = val.name;
+                  }
                 },
               ),
             ),
@@ -87,16 +92,36 @@ class Login extends HookConsumerWidget {
             SizedBox(
               child: FilledButton(
                 onPressed: () async {
-                  final prefs = await SharedPreferences.getInstance();
-                  prefs.setString('name', myAccount.name);
-                  Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(
-                      // 遷移先のクラス
-                      builder: (BuildContext context) =>
-                          TestApp(studentName: myAccount.name),
-                    ),
-                  );
+                  try {
+                    EasyLoading.show(status: '読み込み中');
+                    final prefs = await SharedPreferences.getInstance();
+                    if (ref
+                            .watch(myAccountProvider.notifier)
+                            .state
+                            .runtimeType ==
+                        Student) {
+                      ref.watch(myAccountProvider.notifier).state =
+                          await QueryServiceRepositoryWhichUseFirebase()
+                              .identifyStudent(name: name);
+                    } else {
+                      await QueryServiceRepositoryWhichUseFirebase()
+                          .identifyStuff(name: name);
+                    }
+                    prefs.setString('name',
+                        ref.watch(myAccountProvider.notifier).state.name);
+                    EasyLoading.showSuccess('識別しました');
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                        builder: (BuildContext context) =>
+                            _currentSelection.value == 0
+                                ? TestApp()
+                                : GradesTablePage(),
+                      ),
+                    );
+                  } catch (e) {
+                    EasyLoading.showError('存在しないユーザーです');
+                  }
                 },
                 child: Padding(
                   padding: EdgeInsets.symmetric(vertical: 10, horizontal: 110),
